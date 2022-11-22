d75bbf1fe21c1d99022e4f3b562378f9208f7618
Tus1688 <76937231+Tus1688@users.noreply.github.com>
2022-11-22 11:32:24 +0700

    feat(react-app): print sales invoice

--- react-app/src/components/SalesInvoiceDownload.tsx
+++ react-app/src/components/SalesInvoiceDownload.tsx
@@ -52,7 +52,12 @@ const SalesInvoiceDownload = ({
                             producer='bumbuventory, bumbu47 inventory management system'
                         >
                             <Page size='A4' style={styles.body}>
-                                <View style={{display: 'flex', flexDirection: 'column'}}>
+                                <View
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                    }}
+                                >
                                     <Text style={styles.title}>
                                         Sales Invoice ID: {data?.id}
                                     </Text>
@@ -66,26 +71,215 @@ const SalesInvoiceDownload = ({
                                 <View>
                                     <Text style={styles.title}>
                                         Sales Invoice Date:{' '}
-                                        {new Date(data?.date || '').toLocaleDateString('id-ID')}
+                                        {new Date(
+                                            data?.date || ''
+                                        ).toLocaleDateString('id-ID')}
                                     </Text>
                                     <Text style={styles.title}>
                                         Created By: {data?.created_by}
                                     </Text>
                                 </View>
+                                {/* space for 100px */}
+                                <View style={{ height: '20px' }} />
+                                <View
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'row',
+                                        alignItems: 'stretch',
+                                        border: '1px solid black',
+                                        justifyContent: 'center',
+                                    }}
+                                >
+                                    <Text
+                                        style={{
+                                            width: '20%',
+                                            padding: '3px',
+                                            borderRight: '1px solid black',
+                                        }}
+                                    >
+                                        Name
+                                    </Text>
+                                    <Text
+                                        style={{
+                                            width: '6%',
+                                            padding: '3px',
+                                            borderRight: '1px solid black',
+                                            fontSize: 10,
+                                        }}
+                                    >
+                                        Batch
+                                    </Text>
+                                    <Text
+                                        style={{
+                                            width: '29%',
+                                            padding: '3px',
+                                            borderRight: '1px solid black',
+                                        }}
+                                    >
+                                        Description
+                                    </Text>
+                                    <Text
+                                        style={{
+                                            width: '15%',
+                                            padding: '3px',
+                                            borderRight: '1px solid black',
+                                        }}
+                                    >
+                                        Price
+                                    </Text>
+                                    <Text
+                                        style={{
+                                            width: '6%',
+                                            padding: '3px',
+                                            borderRight: '1px solid black',
+                                        }}
+                                    >
+                                        Disc
+                                    </Text>
+                                    <Text
+                                        style={{
+                                            width: '6%',
+                                            padding: '3px',
+                                            borderRight: '1px solid black',
+                                        }}
+                                    >
+                                        Qty
+                                    </Text>
+                                    <Text
+                                        style={{ width: '18%', padding: '3px' }}
+                                    >
+                                        Total
+                                    </Text>
+                                </View>
                                 <View>
-                                    {/* for each data.items as itemProps map it as table  */} 
                                     {data?.items.map((itemProps) => {
                                         return (
-                                            <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
-                                                <Text>
-                                                    {itemProps.name}{' '}
+                                            <View
+                                                style={{
+                                                    display: 'flex',
+                                                    flexDirection: 'row',
+                                                    alignItems: 'stretch',
+                                                    border: '0.5px solid black',
+                                                    borderLeft:
+                                                        '1px solid black',
+                                                    borderRight:
+                                                        '1px solid black',
+                                                }}
+                                            >
+                                                <Text
+                                                    style={{
+                                                        width: '20%',
+                                                        padding: '3px',
+                                                        borderLeft:
+                                                            '0.5px solid black',
+                                                        borderRight:
+                                                            '1px solid black',
+                                                    }}
+                                                >
+                                                    {itemProps.name}
                                                 </Text>
-                                                <Text>
-                                                    {itemProps.batch_id}{' '}
+                                                <Text
+                                                    style={{
+                                                        width: '6%',
+                                                        padding: '3px',
+                                                        borderRight:
+                                                            '1px solid black',
+                                                    }}
+                                                >
+                                                    {itemProps.batch_id}
+                                                </Text>
+                                                <Text
+                                                    style={{
+                                                        width: '29%',
+                                                        padding: '3px',
+                                                        borderRight:
+                                                            '1px solid black',
+                                                    }}
+                                                >
+                                                    {itemProps.description}
+                                                </Text>
+                                                <Text
+                                                    style={{
+                                                        width: '15%',
+                                                        padding: '3px',
+                                                        borderRight:
+                                                            '1px solid black',
+                                                    }}
+                                                >
+                                                    Rp.{' '}
+                                                    {itemProps.price.toLocaleString(
+                                                        'id-ID'
+                                                    )}
+                                                </Text>
+                                                <Text
+                                                    style={{
+                                                        width: '6%',
+                                                        padding: '3px',
+                                                        borderRight:
+                                                            '1px solid black',
+                                                    }}
+                                                >
+                                                    {itemProps.discount} %
+                                                </Text>
+                                                <Text
+                                                    style={{
+                                                        width: '6%',
+                                                        padding: '3px',
+                                                        borderRight:
+                                                            '1px solid black',
+                                                    }}
+                                                >
+                                                    {itemProps.quantity}
+                                                </Text>
+                                                <Text
+                                                    style={{
+                                                        width: '18%',
+                                                        padding: '3px',
+                                                        borderRight:
+                                                            '0.5px solid black',
+                                                    }}
+                                                >
+                                                    Rp .{' '}
+                                                    {itemProps.total.toLocaleString(
+                                                        'id-ID'
+                                                    )}
                                                 </Text>
                                             </View>
-                                        )
+                                        );
                                     })}
+                                </View>
+                                <View
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'row',
+                                        alignItems: 'stretch',
+                                    }}
+                                >
+                                    <Text
+                                        style={{
+                                            border: '1px solid black',
+                                            width: '76%',
+                                            padding: '3px',
+                                            textAlign: 'center',
+                                        }}
+                                    >
+                                        Subtotal
+                                    </Text>
+                                    <Text
+                                        style={{
+                                            border: '1px solid black',
+                                            borderLeft: '0px',
+                                            width: '24%',
+                                            padding: '3px',
+                                            // center text
+                                            textAlign: 'center',
+                                            // bold font
+                                            fontWeight: 'bold',
+                                        }}
+                                    >
+                                        Rp.{' '}
+                                        {data?.total.toLocaleString('id-ID')}
+                                    </Text>
                                 </View>
                             </Page>
                         </Document>
