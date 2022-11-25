1ae7beb4c38f12a2e56d586617fa1d5ba8fe19ee
Tus1688 <76937231+Tus1688@users.noreply.github.com>
2022-11-25 17:35:46 +0700

    feat(react-app): user list wip

--- react-app/src/routes/UserList.tsx
+++ react-app/src/routes/UserList.tsx
@@ -1,12 +1,58 @@
-import { EuiPageTemplate } from "@elastic/eui";
+import { EuiButton, EuiButtonIcon, EuiFlexGroup, EuiFlexItem, EuiPageTemplate, EuiSpacer, EuiText, EuiTextColor, EuiTitle } from '@elastic/eui';
+import { useState } from 'react';
 
 const UserList = () => {
+    const [modalCreateOpen, setModalCreateOpen] = useState(false);
     return (
         <>
-            <EuiPageTemplate.Section style={{ height: 0}}>
-
+            <EuiPageTemplate.Section>
+                <EuiFlexGroup justifyContent='spaceBetween'>
+                    <EuiFlexItem grow={false}>
+                        <EuiTitle size='l'>
+                            <h1>User List</h1>
+                        </EuiTitle>
+                        <EuiText>
+                            <EuiTextColor color='subdued'>
+                                <p>
+                                    In this page you can see, add, edit and
+                                    delete users.
+                                </p>
+                            </EuiTextColor>
+                        </EuiText>
+                    </EuiFlexItem>
+                    <EuiFlexItem grow={false}>
+                        <EuiFlexGroup direction='row'>
+                            <EuiButton
+                                color='success'
+                                iconType='plusInCircleFilled'
+                                iconSide='right'
+                                onClick={() =>
+                                    setModalCreateOpen(!modalCreateOpen)
+                                }
+                            >
+                                Create
+                            </EuiButton>
+                            <EuiButtonIcon
+                                aria-label='refresh button'
+                                iconType='refresh'
+                                // onClick={() => {
+                                //     setFetchedPage([]);
+                                //     setData([]);
+                                //     setPagination({
+                                //         pageIndex: 0,
+                                //         pageSize: 20,
+                                //     });
+                                // }}
+                                color='primary'
+                                display='base'
+                                size='m'
+                            />
+                        </EuiFlexGroup>
+                    </EuiFlexItem>
+                </EuiFlexGroup>
+                <EuiSpacer size='s' />
             </EuiPageTemplate.Section>
         </>
-    )
+    );
 };
 export default UserList;
